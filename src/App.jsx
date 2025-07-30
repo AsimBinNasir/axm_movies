4a876d38df97d3f60ce2ed2d935c0a48ab0c36f1
AsimBinNasir <asimbinnasir2906@gmail.com>
2025-07-30 21:04:34 +0600

    Add logo and search images; implement Search component
    - Added logo.png and search.png to the public/Images directory.
    - Created a new Search component in src/components/Search.jsx.
    - The Search component includes an input field for searching movies and displays the search icon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import HeroImage from '../public/Images/hero-img.png'
+import Search from './components/Search'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div className='bg-slate-400'>App</div>
+    <main>
+      <div className ="pattern">
+        <div className='wrapper'>
+          <header>
+            <img src ={HeroImage} alt="Hero Banner" />
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassle</h1>
+          </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          
+
+        </div>
+
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SearchIcon from '/public/Images/search.png'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className = "search">
+      <div>
+        <img src={SearchIcon} alt="" />
+
+        <input 
+        type="text"
+        placeholder='Search for thousand of movies...'
+        value={searchTerm} 
+        onChange={(event)=> setSearchTerm(event.target.value)}
+        />
+      </div>
+
+    </div>
+  )
+}
+
+export default Search
